e53e8a4b2b0ee2fb8cc7236ce6dea96ab073bd18
Corina Serna <Corina Serna>
2017-03-08 18:49:07 -0700

    Finished lab.

--- src/_todoInSection/StudentGUI.java
+++ src/_todoInSection/StudentGUI.java
@@ -5,11 +5,15 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.RowSorter;
 import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 @SuppressWarnings("serial")
 public class StudentGUI extends JFrame {
@@ -23,6 +27,7 @@ public class StudentGUI extends JFrame {
   public static void main(String args[]) {
     new StudentGUI().setVisible(true);
   }
+  
 
   // Need a TableModel to set as the model for a JTabel
   private TableModel model = null;
@@ -43,22 +48,29 @@ public class StudentGUI extends JFrame {
     setLocation(30, 30);
 
     // TODO: 2) Need a new StudentCollection as our model
+    model = new StudentCollection();
  
     // TODO: 3) Construct the JTable (table) with our model as an argument (could use setModel)
- 
+    table = new JTable();
+    table.setModel(model);
+    
     // TODO: 4) Construct a JScrollPane to decorate table so that if the data exceeds the 
     // side of the table in the  GUI, then it automatically becomes scrollable.
+    JScrollPane scroll = new JScrollPane(table);
     
     // TODO: 5) Add JScrollPane to this JFrame
-   
+    this.add(scroll, BorderLayout.CENTER); 
     
     // TODO: Run this code to see if the JTable appears (no code to write)
 
     
     // TODO: 6) Construct a new RowSorter<TableModel> to be a TableRowSorter
     // while setting its model to model
- 
-    // TODO: 7) Link up table and the sorter
+    TableRowSorter rowSorter = new TableRowSorter<TableModel>();
+    rowSorter.setModel(model);
+
+    // TODO: 7) link up table and the sorter
+    table.setRowSorter(rowSorter);
  
     // Layout the GUI
     JButton button = new JButton("Select Highlighted Row");
@@ -79,7 +91,7 @@ public class StudentGUI extends JFrame {
       // row need table's getSelectedRow and convertRowIndexToModel as
       // well as model's getValueAt(rowIndex, columnIndex). See the API
       // for details.
-      System.out.println("Get student's name from the table");
+      System.out.println(arg0);
 
     }
   }
